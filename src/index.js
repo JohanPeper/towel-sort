db2ba5abaccb1a54c830ad086f48746dcadf9f7e
Ivan <70844716+JohanPeper@users.noreply.github.com>
2020-10-25 00:47:40 +0300

    feta: redy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (matrix == undefined) {
+        return []
+    }
+    else {
+        for (let i = 1; i < matrix.length; i += 2) {
+            matrix[i] = matrix[i].reverse();
+        }
+        matrix = matrix.flat(Infinity);
+        return matrix;
+    }
 }
