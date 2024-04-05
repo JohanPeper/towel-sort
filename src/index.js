8fce437e3684403a56459bc5e92b91d808037a92
Ivan <70844716+JohanPeper@users.noreply.github.com>
2024-04-05 14:59:34 +0300

    fix: add polyfill

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 
 // You should implement your task here.
-
+    if (!Array.prototype.flat) {
+        Array.prototype.flat = function(depth) {
+          var flattened = [];
+      
+          function flatten(arr, currentDepth) {
+            for (var i = 0; i < arr.length; i++) {
+              if (Array.isArray(arr[i]) && currentDepth < depth) {
+                flatten(arr[i], currentDepth + 1);
+              } else {
+                flattened.push(arr[i]);
+              }
+            }
+          }
+      
+          flatten(this, 0);
+      
+          return flattened;
+        };
+      }
+      // Polyfill ^^^^^
 module.exports = function towelSort(matrix) {
+
+
     if (matrix == undefined) {
         return []
     }
@@ -9,7 +30,7 @@ module.exports = function towelSort(matrix) {
         for (let i = 1; i < matrix.length; i += 2) {
             matrix[i] = matrix[i].reverse();
         }
-        matrix = matrix.flat(Infinity);
-        return matrix;
+
+        return matrix.flat(Infinity);
     }
 }
